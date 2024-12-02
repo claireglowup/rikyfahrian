e884e042e3e29962466e5885dea4da54f04e5edb
claire <claireglowup@gmail.com>
2024-12-02 23:06:28 +0700

    fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import "./styles/App.css";
 
 import { Header } from "./components/Header";
 import { Project } from "./components/Project";
+import { Footer } from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Project />
+      <Footer />
     </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,30 @@
+import "../styles/Footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faInstagram,
+  faYoutube,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export function Footer() {
-  return <></>;
+  return (
+    <div className="footer">
+      <div className="icon">
+        <a href="https://instagram.com/rikyfahrian" target="blank">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a href="https://youtube.com/rikyfahrian" target="blank">
+          <FontAwesomeIcon icon={faYoutube} />
+        </a>
+        <a href="https://linkedin.com/in/rikyfahrian" target="blank">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="mailto:rikyfhrian@gmail.com">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </a>
+      </div>
+      <p>© 2024 Mohamad Riki Fahrian, All Rights Reserved</p>
+    </div>
+  );
 }
